2a3e09181a7ff838647c261b25fd63f98ae79a65
FreezyEx <valeroma200@gmail.com>
2021-06-28 19:01:24 +0200

    Added Whitelist option

--- Airdrop.sol
+++ Airdrop.sol
@@ -139,23 +139,32 @@ contract Airdrop is ReentrancyGuard, Context, Ownable{
     using SafeMath for uint256;
     
     mapping(address => bool) private Claimed;
+    mapping(address => bool) private _isWhitelist;
+    mapping(address => uint256) private _valDrop;
+    
     IERC20 private _token;
     bool airdropLive = false;
-    uint256 valDrop = 0;
     
     event AirdropClaimed(address receiver, uint256 amount);
-
+    event WhitelistSetted(address[] recipient, uint256[] amount);
     
      receive () external payable {
             claimTokens();
      }
      
      //Start Airdrop
-    function startAirdrop(IERC20 tokenAddress, uint256 _valDrop) public onlyOwner{
+    function startAirdrop(IERC20 tokenAddress) public onlyOwner{
         require(airdropLive == false, 'Airdrop already started');
         _token = tokenAddress;
-        valDrop = _valDrop;
         airdropLive = true;
+    }
+    
+     function setWhitelist(address[] calldata recipients, uint256[] calldata amount) external onlyOwner{
+        for(uint i = 0; i< recipients.length; i++){
+            require(recipients[i] != address(0));
+            _valDrop[recipients[i]] = amount[i];
+        }
+        emit WhitelistSetted(recipients, amount);
     }
 
     function claimTokens() public nonReentrant payable {
@@ -163,7 +172,7 @@ contract Airdrop is ReentrancyGuard, Context, Ownable{
         require(Claimed[msg.sender] == false, 'Airdrop already claimed!');
         if(_token.balanceOf(address(this)) == 0) { airdropLive = false; return;}
         Claimed[msg.sender] = true;
-        uint256 amount = valDrop.mul(10**9);
+        uint256 amount = _valDrop[msg.sender].mul(10**9);
         _token.transfer(msg.sender, amount);
         emit AirdropClaimed(msg.sender, amount);
     }
@@ -172,6 +181,14 @@ contract Airdrop is ReentrancyGuard, Context, Ownable{
          require(address(this).balance > 0, 'Contract has no money');
          address payable wallet = payable(msg.sender);
         wallet.transfer(address(this).balance);    
+    }
+    
+    function takeTokens(IERC20 tokenAddress)  public onlyOwner{
+        IERC20 tokenBEP = tokenAddress;
+        uint256 tokenAmt = tokenBEP.balanceOf(address(this));
+        require(tokenAmt > 0, 'BEP-20 balance is 0');
+        address payable wallet = payable(msg.sender);
+        tokenBEP.transfer(wallet, tokenAmt);
     }
 
 }
